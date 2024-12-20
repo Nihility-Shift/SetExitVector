ecd17d4fb5fda509f7bfbccc8824ec859563c220
DragonFire47 <46509577+DragonFire47@users.noreply.github.com>
2024-12-19 16:15:28 -0800

    Fixed referenced fields for 1.0.3

--- SetExitVector/VoidDriveModulePatch.cs
+++ SetExitVector/VoidDriveModulePatch.cs
@@ -5,14 +5,15 @@ using Photon.Pun;
 
 namespace SetExitVector
 {
+    //If all engines are charging, assign exit vector when last engine is set to start charging..
     [HarmonyPatch(typeof(VoidDriveModule), "SetEngineCharging")]
     internal class VoidDriveModulePatch
     {
-        static void Postfix(VoidDriveModule __instance, VoidJumpSystem ___voidJumpSystem, bool[] ___engineStates)
+        static void Postfix(VoidDriveModule __instance, VoidJumpSystem ___voidJumpSystem, bool[] ___engineChargedStates)
         {
             //Host, config, vector already set, interdiction
             if (!PhotonNetwork.IsMasterClient || !PluginConfig.ExitVector.Value || ___voidJumpSystem.IsExitVectorSet() || __instance.VoidJumpCapable.Value <= 0) return;
-            foreach (bool state in ___engineStates) if (!state) return;
+            foreach (bool state in ___engineChargedStates) if (!state) return;
             ___voidJumpSystem.ExitVectorSet(true);
         }
     }
